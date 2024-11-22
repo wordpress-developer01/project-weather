a24a14bf746b2c846e7f4675c066746b371cfc17
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2024-11-22 17:37:14 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+let kelvin = 293;
+// the kevin weather 293
+let celsius = kelvin - 273;
+// the celsius weather 
+let fahrenheit = celsius * (9/5) + 32;
+//  fahrenheitthe 
+Math.floor(fahrenheit);
+// the floor method
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
